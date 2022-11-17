250bc48cf2dc3f5b89f865f88e8b7eb93ce48e4d
zwug <zwugme@gmail.com>
2022-11-17 19:46:51 +0300

    added dotenv

--- index.ts
+++ index.ts
@@ -1,18 +1,22 @@
 import express from 'express';
 import cors from 'cors';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const port = 8000;
 
-var corsOptions = {
-  origin: 'http://localhost:3000',
+const corsOptions = {
+  origin: `${process.env.HOST}:3000`, // frontend dev server
   optionsSuccessStatus: 200
 }
 
-app.get('/api/test', cors(corsOptions), (_, res) => {
+app.use(cors(corsOptions));
+
+app.get('/api/test', (_, res) => {
   res.send(200);
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
 })
